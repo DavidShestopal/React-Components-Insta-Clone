ab3a396916d1f1652be565d770c28e7531747107
David Shestopal <dshestopal@yahoo.com>
2020-02-18 13:54:56 -0800

    push

--- src/components/CommentSection/CommentSectionContainer.js
+++ src/components/CommentSection/CommentSectionContainer.js
@@ -1,15 +1,21 @@
 // You will add code to this file
-import React, { useState } from "react";
-import CommentInput from "./CommentInput";
-import Comment from "./Comment";
-import "./Comment.css";
+import React, { useState } from 'react';
+import CommentInput from './CommentInput';
+import Comment from './Comment';
+import './Comment.css';
 
 const CommentSection = props => {
   // Add state for the comments
+  console.log('commentsProps', props.comments);
+
+  const [comments] = useState(props.comments);
 
   return (
     <div>
       {/* map through the comments data and return the Comment component */}
+      {comments.map((comment, index) => {
+        return <Comment comment={comment} key={index} />;
+      })}
       <CommentInput />
     </div>
   );
--- src/components/PostsContainer/Post.js
+++ src/components/PostsContainer/Post.js
@@ -1,39 +1,32 @@
 // You will add code in this file
-import React from "react";
-import CommentSection from "../CommentSection/CommentSectionContainer";
-import LikeSection from "./LikeSection";
-import PostHeader from "./PostHeader";
+import React, { useState } from 'react';
+import CommentSection from '../CommentSection/CommentSectionContainer';
+import LikeSection from './LikeSection';
+import PostHeader from './PostHeader';
 
-import "./Posts.css";
+import './Posts.css';
 
 // pass props in this file to
 const Post = props => {
   // set up state for the likes
+  const [numberOfLikes, setNumberOfLikes] = useState(0);
+  console.log(numberOfLikes);
+
+  function likePlusOne() {
+    setNumberOfLikes(numberOfLikes + 1);
+  }
 
   return (
     <div className="post-border">
-      <PostHeader
-        username={props.post.username}
-        thumbnailUrl={
-          props.post.thumbnailUrl
-        }
-      />
+      <PostHeader username={props.post.username} thumbnailUrl={props.post.thumbnailUrl} />
       <div className="post-image-wrapper">
-        <img
-          alt="post thumbnail"
-          className="post-image"
-          src={props.post.imageUrl}
-        />
+        <img alt="post thumbnail" className="post-image" src={props.post.imageUrl} />
       </div>
-      <LikeSection />
-      <CommentSection
-        postId={props.post.imageUrl}
-        comments={props.post.comments}
-      />
+
+      <LikeSection numberOfLikes={numberOfLikes} likeFunc={likePlusOne} />
+      <CommentSection postId={props.post.imageUrl} comments={props.post.comments} />
     </div>
   );
 };
 
 export default Post;
-
-
